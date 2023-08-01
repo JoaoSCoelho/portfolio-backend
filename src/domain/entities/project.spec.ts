7933827e6dcdff908e5e1af8615dc33966b5f097
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-01 20:32:44 -0300

    ✨ create entity project

---
+++ src/domain/entities/project.spec.ts
@@ -0,0 +1,343 @@
+import { randCompanyName, randParagraph, randUrl } from '@ngneat/falso'
+import { randomUUID } from 'crypto'
+
+import { Left, isLeft } from '../../shared/either'
+import { Project } from './project'
+
+describe('Instanciando um novo Project', () => {
+  describe('Instanciando com sucesso', () => {
+    test('Deve instanciar um novo Project', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+      }
+
+      const project = Project.create(projectData.name, projectData.description)
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual({
+        id: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        name: projectData.name,
+        description: projectData.description,
+      })
+    })
+
+    test('Deve instanciar um novo Project com link', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+        link: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        projectData.link,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual({
+        id: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        name: projectData.name,
+        description: projectData.description,
+        link: projectData.link,
+      })
+    })
+
+    test('Deve instanciar um novo Project com repositoryUrl', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+        repositoryUrl: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        projectData.repositoryUrl,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual({
+        id: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        name: projectData.name,
+        description: projectData.description,
+        repositoryUrl: projectData.repositoryUrl,
+      })
+    })
+
+    test('Deve instanciar um novo Project com repositoryUrl e link', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+        repositoryUrl: randUrl(),
+        link: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        projectData.repositoryUrl,
+        projectData.link,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual({
+        id: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        name: projectData.name,
+        description: projectData.description,
+        repositoryUrl: projectData.repositoryUrl,
+        link: projectData.link,
+      })
+    })
+  })
+
+  describe('Retornando mensagem de incompatibilidade', () => {
+    test('Deve retornar um erro do name', () => {
+      const projectData = {
+        name: 546543,
+        description: randParagraph(),
+      }
+
+      const project = Project.create(
+        projectData.name as any,
+        projectData.description,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+
+    test('Deve retornar um erro do description', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: 54654132,
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description as any,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+
+    test('Deve retornar um erro do repositoryUrl', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+        repositoryUrl: 'no-formatted-url',
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        projectData.repositoryUrl,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+
+    test('Deve retornar um erro do link', () => {
+      const projectData = {
+        name: randCompanyName(),
+        description: randParagraph(),
+        link: 'no-formatted-url',
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        projectData.link,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+  })
+})
+
+// -------------------------------------------------------------------------
+
+describe('Instanciando um Project existente', () => {
+  describe('Instanciando com sucesso', () => {
+    test('Deve instanciar um project', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        undefined,
+        projectData.id,
+        projectData.createdAt,
+        projectData.updatedAt,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual(projectData)
+    })
+
+    test('Deve instanciar um project com repositoryUrl', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+        repositoryUrl: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        projectData.repositoryUrl,
+        undefined,
+        projectData.id,
+        projectData.createdAt,
+        projectData.updatedAt,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual(projectData)
+    })
+
+    test('Deve instanciar um project com link', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+        link: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        projectData.link,
+        projectData.id,
+        projectData.createdAt,
+        projectData.updatedAt,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual(projectData)
+    })
+
+    test('Deve instanciar um project com repositoryUrl e link', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+        repositoryUrl: randUrl(),
+        link: randUrl(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        projectData.repositoryUrl,
+        projectData.link,
+        projectData.id,
+        projectData.createdAt,
+        projectData.updatedAt,
+      )
+
+      expect(project).toBeInstanceOf(Project)
+      expect(project).toEqual(projectData)
+    })
+  })
+
+  describe('Retornando mensagem de incompatibilidade', () => {
+    test('Deve retornar um erro do id', () => {
+      const projectData = {
+        id: 45454654654,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        undefined,
+        projectData.id as any,
+        projectData.createdAt,
+        projectData.updatedAt,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+
+    test('Deve retornar um erro do createdAt', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: 'new Date()',
+        updatedAt: new Date(),
+        name: randCompanyName(),
+        description: randParagraph(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        undefined,
+        projectData.id,
+        projectData.createdAt as any,
+        projectData.updatedAt,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+
+    test('Deve retornar um erro do updatedAt', () => {
+      const projectData = {
+        id: randomUUID(),
+        createdAt: new Date(),
+        updatedAt: 'new Date()',
+        name: randCompanyName(),
+        description: randParagraph(),
+      }
+
+      const project = Project.create(
+        projectData.name,
+        projectData.description,
+        undefined,
+        undefined,
+        projectData.id,
+        projectData.createdAt,
+        projectData.updatedAt as any,
+      )
+
+      expect(isLeft(project)).toBeTruthy()
+      expect(typeof (project as Left<string>).value).toBe('string')
+    })
+  })
+})
--- src/domain/entities/project.ts
+++ src/domain/entities/project.ts
@@ -1,23 +1,69 @@
-import { Entity } from '../../core/domain/Entity'
+import { Entity, EntityDTO, EntityParams } from '../../core/domain/entity'
+import { Either, isLeft } from '../../shared/either'
+import { ID } from '../aggregates/value-objects/id'
+import { Moment } from '../aggregates/value-objects/moment'
+import { ProjectDescription } from '../aggregates/value-objects/project-description'
+import { ProjectName } from '../aggregates/value-objects/project-name'
+import { WebUrl } from '../aggregates/value-objects/web-url'
 
-interface IProjectProps {
-  readonly name: ProjectName
-  readonly description: ProjectDescription
-  readonly repositoryURL?: WebUrl
-  readonly link?: WebUrl
-}
-
-export class Project extends Entity implements IProjectProps {
+export class Project extends Entity {
   private constructor(
     public readonly name: ProjectName,
     public readonly description: ProjectDescription,
-    public readonly repositoryURL?: WebUrl,
+    public readonly repositoryUrl?: WebUrl,
     public readonly link?: WebUrl,
-    ...params: Required<ConstructorParameters<typeof Entity>> | []
+    ...params: EntityParams
   ) {
     super(...params)
     Object.freeze(this)
   }
 
-  static create() {}
+  static create(
+    _name: string,
+    _description: string,
+    _repositoryUrl?: string,
+    _link?: string,
+    ...params: EntityDTO
+  ): Either<string, Project> {
+    // Verifica para uma nova entidade ----------------------------------------
+
+    const name = ProjectName.create(_name)
+    if (isLeft(name)) return name
+
+    const description = ProjectDescription.create(_description)
+    if (isLeft(description)) return description
+
+    const repositoryUrl =
+      _repositoryUrl !== undefined ? WebUrl.create(_repositoryUrl) : undefined
+    if (isLeft(repositoryUrl)) return repositoryUrl
+
+    const link = _link !== undefined ? WebUrl.create(_link) : undefined
+    if (isLeft(link)) return link
+
+    if (!params.length)
+      return new Project(name, description, repositoryUrl, link)
+
+    // Verifica para uma entidade já existente ----------------------------------------
+
+    const [_id, _createdAt, _updatedAt] = params
+
+    const id = ID.create(_id)
+    if (isLeft(id)) return id
+
+    const createdAt = Moment.create(_createdAt)
+    if (isLeft(createdAt)) return createdAt
+
+    const updatedAt = Moment.create(_updatedAt)
+    if (isLeft(updatedAt)) return updatedAt
+
+    return new Project(
+      name,
+      description,
+      repositoryUrl,
+      link,
+      id,
+      createdAt,
+      updatedAt,
+    )
+  }
 }
