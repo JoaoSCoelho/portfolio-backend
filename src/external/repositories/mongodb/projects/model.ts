9ff564198f82bf516051a46dc3352a98cfc42d04
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-02 20:26:25 -0300

    🐛 fix Project model bug

--- src/external/repositories/mongodb/projects/model.ts
+++ src/external/repositories/mongodb/projects/model.ts
@@ -4,26 +4,16 @@ import { BaseSchema } from '..'
 
 export const projectSchema = new BaseSchema(
   {
-    description: {
-      type: String,
-      required: false,
-    },
     name: {
       type: String,
       required: true,
     },
-    spent: {
-      type: Number,
-      required: true,
-    },
-    bankAccountId: {
+    description: {
       type: String,
       required: true,
     },
-    createdTimestamp: {
-      type: Number,
-      required: true,
-    },
+    repositoryUrl: String,
+    link: String,
   },
   {
     timestamps: true,
