d63e756d1c80625eedff75503bc62f972f4b54ca
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-03 07:26:54 -0300

    🔧 add config in jest

---
+++ tests/e2e/jest-setup.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI!)
+})
+
+afterAll(async () => {
+  await Promise.all(
+    mongoose
+      .modelNames()
+      .map(async (modelName) => await mongoose.model(modelName).deleteMany()),
+  )
+  await mongoose.disconnect()
+})
