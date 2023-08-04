e8740a90971ff879c8dc92f0c07eb63a3a0ffa9b
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-03 21:04:53 -0300

    🔧 update jest-setup on e2e tests

--- tests/e2e/jest-setup.ts
+++ tests/e2e/jest-setup.ts
@@ -1,19 +1,11 @@
 import mongoose from 'mongoose'
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URI!)
-})
-
-beforeEach(async () => {
   if (!mongoose.connection.readyState)
     await mongoose.connect(process.env.MONGO_URI!)
-})
-
-afterAll(async () => {
   await Promise.all(
     mongoose
       .modelNames()
       .map(async (modelName) => await mongoose.model(modelName).deleteMany()),
   )
-  await mongoose.disconnect()
 })
